946913f06890be903c428ccd681f2fab6f067c4f
kamilowski55555 <kamilkot55555@gmail.com>
2023-08-20 18:05:13 +0200

    Project 1.0 without lombok

---
+++ src/main/java/org/example/StudentComparator.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Comparator;
+
+public class StudentComparator {
+    public static class FirstNameComparator implements Comparator<Student> {
+        @Override
+        public int compare(Student s1, Student s2) {
+            return s1.getFirstName().compareTo(s2.getFirstName());
+        }
+    }
+    public static class LastNameComparator implements Comparator<Student> {
+        @Override
+        public int compare(Student s1, Student s2) {
+            return s1.getLastName().compareTo(s2.getLastName());
+        }
+    }
+    public static class PeselComparator implements Comparator<Student> {
+        @Override
+        public int compare(Student s1, Student s2) {
+            return s1.getPesel().compareTo(s2.getPesel());
+        }
+    }
+
+}
